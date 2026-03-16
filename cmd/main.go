38dbc066b1c821cbf5d02b265faa8842b5387c5c
Mohamed Aly <troy100100@gmail.com>
2026-03-17 01:30:36 +0200

    chore: init server with postgres and redis connections

--- cmd/main.go
+++ cmd/main.go
@@ -1,16 +1,73 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	"github.com/joho/godotenv"
+	"github.com/redis/go-redis/v9"
 )
 
 func main() {
-	s := "gopher"
-	fmt.Printf("Hello and welcome, %s!\n", s)
+	godotenv.Load()
 
-	for i := 1; i <= 5; i++ {
-		//TIP <p>To start your debugging session, right-click your code in the editor and select the Debug option.</p> <p>We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-		// for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.</p>
-		fmt.Println("i =", 100/i)
+	port := os.Getenv("PORT")
+	dbURL := os.Getenv("DATABASE_URL")
+	redisURL := os.Getenv("REDIS_URL")
+
+	// Postgres
+	var pool *pgxpool.Pool
+	var err error
+
+	for i := range 5 {
+		pool, err = pgxpool.New(context.Background(), dbURL)
+		if err == nil {
+			err = pool.Ping(context.Background())
+		}
+		if err == nil {
+			break
+		}
+		log.Printf("DB not ready, attempt %d/5 - retrying in 2s", i+1)
+		time.Sleep(2 * time.Second)
+	}
+
+	if err != nil {
+		log.Fatalf("Could not connect to database: %s", err)
+	}
+
+	defer pool.Close()
+	log.Println("Connected to Postgres")
+
+	// Redis
+	rdb := redis.NewClient(&redis.Options{Addr: redisURL})
+
+	if err := rdb.Ping(context.Background()).Err(); err != nil {
+		log.Fatalf("Could not connect to redis: %s", err)
+	}
+
+	defer rdb.Close()
+	log.Println("Connected to Redis")
+
+	// Routes
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /generate", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("POST /generate", r.URL.Path)
+	})
+
+	mux.HandleFunc("GET /{code}", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println("GET /{code}", r.URL.Path)
+	})
+
+	// Server
+	addr := fmt.Sprintf(":%s", port)
+	log.Printf("server starting on %s", addr)
+	if err := http.ListenAndServe(addr, mux); err != nil {
+		log.Fatalf("server error: %v", err)
 	}
 }
